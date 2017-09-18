d11ad553d009a9ee2f27f1ea8538fba8ee8e5537
Manan Thakkar <thakkar1016@gmail.com>
2017-09-18 12:33:38 -0400

    updating views to use django render shortcut

--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.shortcuts import render
 
 def index(request):
-    template = loader.get_template('index.html')
-    context = RequestContext(request, {'testdata': 'Template Var Test'})
-    return HttpResponse(template.render(context))
+    #template = loader.get_template('index.html')
+    #context = RequestContext(request, {'testdata': 'Template Var Test'})
+    #return HttpResponse(template.render(context))
+    context = {'testdata' : 'Template Var Test'}
+    return render(request, 'index.html', context)
+
